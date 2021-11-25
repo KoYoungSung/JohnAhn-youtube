95e37ded8198e044f0236387210504a8f26f7201
KoYoungSung <ken3650@naver.com>
2021-11-25 14:12:18 +0900

    썸네일완료

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
-import Dropzone from 'react-dropzone'
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
+// import { useSelector } from "react-redux";
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -23,6 +25,9 @@ function VideoUploadPage() {
   const [Description, setDescription] = useState('');
   const [Private, setPrivate] = useState(0);
   const [Category, setCategory] = useState('영화 & 애니');
+  const [FilePath, setFilePath] = useState("")
+  const [Duration, setDuration] = useState("")
+  const [Thumbnail, setThumbnail] = useState("")
 
   const onTitleChange = (e) => {
     setVideoTitle(e.currentTarget.value)
@@ -31,77 +36,110 @@ function VideoUploadPage() {
   const onDescriptionChange = (e) => {
     setDescription(e.currentTarget.value)
   }
-  
+
   const onPrivateChange = (e) => {
     setPrivate(e.currentTarget.value)
   }
-  
+
   const onCategoryChange = (e) => {
     setCategory(e.currentTarget.value)
   }
-  
+
+  const onSubmit = function () {
+
+  }
+
+  const onDrop = (files) => {
+
+    let formData = new FormData();
+    const config = {
+      header: { 'content-type': 'multipart/form-data' }
+    }
+    console.log(files)
+    formData.append("file", files[0])
+
+    axios.post('/api/video/uploadfiles', formData, config)
+      .then(response => {
+        if (response.data.success) {
+
+          let variable = {
+            filePath: response.data.filePath,
+            fileName: response.data.fileName
+          }
+          setFilePath(response.data.filePath)
+
+          //gerenate thumbnail with this filepath ! 
+
+          axios.post('/api/video/thumbnail', variable)
+            .then(response => {
+              if (response.data.success) {
+                setDuration(response.data.fileDuration)
+                setThumbnail(response.data.thumbsFilePath)
+              } else {
+                alert('Failed to make the thumbnails');
+              }
+            })
+
+
+        } else {
+          alert('failed to save the video in server')
+        }
+      })
+
+  }
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
-        <Title level={2}>Upload Video</Title>
+        <Title level={2} > Upload Video</Title>
       </div>
 
-      <Form obSubmit>
+      <Form onSubmit={onSubmit}>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-
-          {/* 드랍 존 */}
           <Dropzone
-            onDrop
-            multiple
-            maxSize>
+            onDrop={onDrop}
+            multiple={false}
+            maxSize={800000000}>
             {({ getRootProps, getInputProps }) => (
-              <div style={{
-                width: '300px', height: '240px', display: 'flex', border: '1px solid lightgray',
-                alignItems: 'center', justifyContent: 'center'
-              }} {...getRootProps()}>
+              <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                {...getRootProps()}
+              >
                 <input {...getInputProps()} />
                 <Icon type="plus" style={{ fontSize: '3rem' }} />
+
               </div>
             )}
-
-
           </Dropzone>
-
-          {/* 썸네일  */}
-          <div>
-            <img src alt />
-          </div>
-
+          {Thumbnail !== "" &&
+            <div>
+              <img src={`http://localhost:5000/${Thumbnail}`} alt="haha" />
+            </div>
+          }
         </div>
 
-        <br />
-        <br />
+
+        <br /><br />
         <label>Title</label>
         <Input
           onChange={onTitleChange}
           value={VideoTitle}
         />
 
-        <br />
-        <br />
-
-        <label>Descriptiton</label>
+        <br /><br />
+        <label>Description</label>
         <TextArea
           onChange={onDescriptionChange}
           value={Description}
         />
 
-        <br />
-        <br />
-
+        <br /><br />
         <select onChange={onPrivateChange}>
           {PrivateOptions.map((item, index) => (
             <option key={index} value={item.label}>{item.label}</option>
           ))}
         </select>
 
-        <br />
-        <br />
+        <br /><br />
 
         <select onChange={onCategoryChange}>
           {CategoryOptions.map((item, index) => (
@@ -109,10 +147,9 @@ function VideoUploadPage() {
           ))}
         </select>
 
-        <br />
-        <br />
+        <br /><br />
 
-        <Button type="primary" size="large" onClick>
+        <Button type="primary" size="large" onClick={onSubmit}>
           Submit
         </Button>
 
